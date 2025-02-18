b7354f8f062db46f6e3600e66f58079dd6cf505f
Maycon_M <maycon.fernandes9@gmail.com>
2025-02-17 22:51:19 -0300

    criando conexão e entidade do db

---
+++ src/configs/database.py
@@ -0,0 +1,5 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import os
+
+DATABASE_URL = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST', 'localhost')}:{os.getenv('POSTGRES_PORT', '5432')}/{os.getenv('POSTGRES_DB')}"
---
+++ src/models/entities/lembrete.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String, Time
+from src.models.base import Base
+
+class Lembrete(Base):
+    __tablename__ = "lembretes"
+
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    nome = Column(String(255), nullable=False)
+    quantidade = Column(Integer, nullable=False)
+    horario = Column(Time, nullable=False)
